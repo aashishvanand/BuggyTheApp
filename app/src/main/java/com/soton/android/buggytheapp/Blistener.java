c4f69832031b385c4ee8070eb53850b854191a7a
Aashish Vivekanand <aashish@Aashishs-MacBook-Pro.local>
2018-06-24 09:43:19 +0530

    Initial commit

---
+++ app/src/main/java/com/soton/android/buggytheapp/Blistener.java
@@ -0,0 +1,18 @@
+package com.soton.android.buggytheapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Blistener extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final Bundle bundle = intent.getExtras();
+        //if   (intent.getAction().equalsIgnoreCase("com.soton.android.buggytheapp"))
+        Toast.makeText(context,bundle.getString("Message"),Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/soton/android/buggytheapp/broadcaster.java
@@ -0,0 +1,31 @@
+package com.soton.android.buggytheapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class broadcaster extends IntentService {
+    //this class do not have default constructor
+    public broadcaster()
+    {
+        super("mybroadcaster");
+    }
+    public static boolean broadcasting=false;
+
+    @Override
+    protected void onHandleIntent(Intent intent)
+    {
+        while ( broadcasting) {
+            Intent broadcastintent = new Intent();
+            broadcastintent.setAction("com.soton.android.bugysqliteapp");
+            broadcastintent.putExtra("Message", "This is always running service");
+            sendBroadcast(broadcastintent);
+            try
+            {
+                Thread.sleep(3000);
+            }catch (Exception ex){}
+
+
+        }
+
+    }
+}
